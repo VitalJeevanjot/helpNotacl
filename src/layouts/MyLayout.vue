a91f99b109b5c9ec59df736811cd6709f2d9d5e5
Genievot <40867747+genievot@users.noreply.github.com>
2019-03-13 05:48:10 +0530

    nota cl help website

---
+++ src/layouts/MyLayout.vue
@@ -0,0 +1,32 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated class="glossy">
+      <q-toolbar>
+        <q-avatar>
+          <img src="statics/logoNotaCL Inkscape.png"/>
+        </q-avatar>
+        <q-toolbar-title>
+          NotaCL Pro
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+export default {
+  name: 'MyLayout',
+  data () {
+    return {
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/pages/Index.vue
@@ -0,0 +1,139 @@
+<template>
+  <q-page class="flex flex-center">
+    <div class="q-pa-md" style="max-width: 350px">
+  <q-list bordered class="rounded-borders">
+    <q-expansion-item label="How to edit room title and color: Nota CL Pro">
+      <q-card>
+        <q-card-section>
+          <div class="q-pa-md q-gutter-md">
+            <q-video
+              src="https://www.youtube.com/embed/xzudWr0sRy4"
+            />
+          </div>
+        </q-card-section>
+      </q-card>
+    </q-expansion-item>
+
+    <q-expansion-item label="How to change Header color: Nota CL Pro">
+      <q-card>
+        <q-card-section>
+          <div class="q-pa-md q-gutter-md">
+            <q-video
+              src="https://www.youtube.com/embed/-zs4TiAeJo4"
+            />
+          </div>
+        </q-card-section>
+      </q-card>
+    </q-expansion-item>
+
+    <q-expansion-item label="How to create new Chat Room: Nota CL Pro">
+      <q-card>
+        <q-card-section>
+          <div class="q-pa-md q-gutter-md">
+            <q-video
+              src="https://www.youtube.com/embed/SMz0xvwzKOw"
+            />
+          </div>
+        </q-card-section>
+      </q-card>
+    </q-expansion-item>
+
+    <q-expansion-item label="How to write notes inside rooms: Nota CL Pro">
+      <q-card>
+        <q-card-section>
+          <div class="q-pa-md q-gutter-md">
+            <q-video
+              src="https://www.youtube.com/embed/PyoL_csfUDg"
+            />
+          </div>
+        </q-card-section>
+      </q-card>
+    </q-expansion-item>
+
+    <q-expansion-item label="How to unhide room: Nota CL Pro" >
+      <q-card>
+        <q-card-section>
+          <div class="q-pa-md q-gutter-md">
+            <q-video
+              src="https://www.youtube.com/embed/5meG7RT12BU"
+            />
+          </div>
+        </q-card-section>
+      </q-card>
+    </q-expansion-item>
+
+    <q-expansion-item label="How to edit notes in a room: Nota CL Pro">
+      <q-card>
+        <q-card-section>
+          <div class="q-pa-md q-gutter-md">
+            <q-video
+              src="https://www.youtube.com/embed/WNdWsJpp_Js"
+            />
+          </div>
+        </q-card-section>
+      </q-card>
+    </q-expansion-item>
+
+    <q-expansion-item label="How to hide room: Nota CL Pro">
+      <q-card>
+        <q-card-section>
+          <div class="q-pa-md q-gutter-md">
+            <q-video
+              src="https://www.youtube.com/embed/nApLyPgkI00"
+            />
+          </div>
+        </q-card-section>
+      </q-card>
+    </q-expansion-item>
+
+    <q-expansion-item label="How to delete notes from room: Nota CL Pro">
+      <q-card>
+        <q-card-section>
+          <div class="q-pa-md q-gutter-md">
+            <q-video
+              src="https://www.youtube.com/embed/nApLyPgkI00"
+            />
+          </div>
+        </q-card-section>
+      </q-card>
+    </q-expansion-item>
+
+    <q-expansion-item label="How to Delete Chat Room : Nota CL Pro">
+      <q-card>
+        <q-card-section>
+          <div class="q-pa-md q-gutter-md">
+            <q-video
+              src="https://www.youtube.com/embed/urvcYShL6RQ"
+            />
+          </div>
+        </q-card-section>
+      </q-card>
+    </q-expansion-item>
+
+    <q-expansion-item icon="cloud" header-class="text-blue" label="How to Delete Chat Room : Nota CL Pro">
+      <q-card>
+        <q-card-section>
+          <div class="q-pa-md q-gutter-md">
+            <span class="text-subtitle2">Pull to overwrite current rooms and data with cloud saved data. Use Pull to refresh feature that will ask to whether you want to overwrite data from cloud or not.</span>
+          </div>
+        </q-card-section>
+      </q-card>
+    </q-expansion-item>
+  </q-list>
+  <div class="row justify-center">
+    <a href="#"></a>
+    <img src="statics/google-play-badge.png" style="width: 300px; height: 120px"/>
+  </div>
+
+</div>
+  </q-page>
+</template>
+
+<style>
+</style>
+
+<script>
+export default {
+  name: 'PageIndex'
+}
+</script>
